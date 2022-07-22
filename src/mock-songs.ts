3460b1893da3cc728833f286ffd2893def94d3a6
Gary Dong <garyd89@bu.edu>
2022-07-22 15:39:38 -0400

    made some changes

--- src/mock-songs.ts
+++ src/mock-songs.ts
@@ -2,6 +2,8 @@ import { Song } from "./songs";
 
 export const SONGS: Song[] = [
     {index: "1", imageURL: "/Users/garydong/Tracklist (FINAL)/FanPad-Tracklist/src/assets/default_100.png", name: "Lorem ipsum", artist: "unknown" , timestamp: "3:32", status: true },
+    {index: "2", imageURL: "/Users/garydong/Tracklist (FINAL)/FanPad-Tracklist/src/assets/default_100.png", name: "Lorem ipsum", artist: "unknown", timestamp: "2:51", status:false},
+    {index: "1", imageURL: "/Users/garydong/Tracklist (FINAL)/FanPad-Tracklist/src/assets/default_100.png", name: "Lorem ipsum", artist: "unknown" , timestamp: "3:32", status: true },
     {index: "2", imageURL: "/Users/garydong/Tracklist (FINAL)/FanPad-Tracklist/src/assets/default_100.png", name: "Lorem ipsum", artist: "unknown", timestamp: "2:51", status:false}
 
 ] 
