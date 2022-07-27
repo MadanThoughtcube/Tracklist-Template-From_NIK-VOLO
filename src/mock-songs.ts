4d6ec3205d9a5884f52df1defe87b75453d4b188
Gary Dong <garyd89@bu.edu>
2022-07-27 12:13:23 -0400

    fixed indexing

--- src/mock-songs.ts
+++ src/mock-songs.ts
@@ -3,7 +3,7 @@ import { Song } from "./songs";
 export const SONGS: Song[] = [
     {index: "1", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist" , timestamp: "3:32", status: true },
     {index: "2", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist", timestamp: "2:51", status:false},
-    {index: "1", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist" , timestamp: "3:32", status: true },
-    {index: "2", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist", timestamp: "2:51", status:false}
+    {index: "3", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist" , timestamp: "3:32", status: true },
+    {index: "4", imageURL: "/Users/garydong/Desktop/Tracklist Template/src/assets/img/unknown.jpg", name: "song", artist: "artist", timestamp: "2:51", status:false}
 
 ] 
